224563a966de4b3e271aeda0f994cee7ca3d4b82
Dinis Cruz <dinis.cruz@owasp.org>
2025-08-05 15:42:06 +0100

    added service title and version to Swagger

--- mgraph_ai_service_base/config.py
+++ mgraph_ai_service_base/config.py
@@ -1,4 +1,5 @@
 from mgraph_ai_service_base import package_name
 
 SERVICE_NAME                             = package_name
+FAST_API__TITLE                          = "FastAPI Service"                                # replace with actual service name
 LAMBDA_DEPENDENCIES__FAST_API_SERVERLESS = ['osbot-fast-api-serverless==v1.2.0']
--- mgraph_ai_service_base/fast_api/Service__Fast_API.py
+++ mgraph_ai_service_base/fast_api/Service__Fast_API.py
@@ -1,8 +1,27 @@
 from osbot_fast_api_serverless.fast_api.Serverless__Fast_API import Serverless__Fast_API
+from mgraph_ai_service_base.config                           import FAST_API__TITLE
 from mgraph_ai_service_base.fast_api.routes.Routes__Info     import Routes__Info
+from mgraph_ai_service_base.utils.Version                    import version__mgraph_ai_service_base
+
 
 
 class Service__Fast_API(Serverless__Fast_API):
 
+    def fast_api__title(self):                                       # todo: move this to the Fast_API class
+        return FAST_API__TITLE
+    
+    def setup(self):
+        super().setup()
+        self.setup_fast_api_title_and_version()
+        return self
+
+    def setup_fast_api_title_and_version(self):                     # todo: move this to the Fast_API class
+        app       = self.app()
+        app.title = self.fast_api__title()
+        app.version = version__mgraph_ai_service_base
+        return self
     def setup_routes(self):
         self.add_routes(Routes__Info  )
+
+
+
