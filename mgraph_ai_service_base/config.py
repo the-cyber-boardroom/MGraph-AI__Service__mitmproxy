19229366102978d2a408421abbb86be518f42e4a
Dinis Cruz <dinis.cruz@owasp.org>
2025-08-05 19:19:04 +0100

    final set of changes before doing v1.0.0 release

--- mgraph_ai_service_base/config.py
+++ mgraph_ai_service_base/config.py
@@ -1,5 +1,6 @@
 from mgraph_ai_service_base import package_name
 
 SERVICE_NAME                             = package_name
-FAST_API__TITLE                          = "FastAPI Service"                                # replace with actual service name
+FAST_API__TITLE                          = "MGraph-AI Service Base"
+FAST_API__DESCRIPTION                    = "Base template for MGraph-AI microservices"
 LAMBDA_DEPENDENCIES__FAST_API_SERVERLESS = ['osbot-fast-api-serverless==v1.2.0']
