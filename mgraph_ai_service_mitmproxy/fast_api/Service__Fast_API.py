b52fac0774e77f5107966714ac9c4fb1fa63c4b4
Dinis Cruz <dinis.cruz@owasp.org>
2025-09-05 10:59:26 +0100

    disabled API key

--- mgraph_ai_service_mitmproxy/fast_api/Service__Fast_API.py
+++ mgraph_ai_service_mitmproxy/fast_api/Service__Fast_API.py
@@ -8,6 +8,7 @@ from mgraph_ai_service_mitmproxy.utils.Version                    import version
 
 
 class Service__Fast_API(Serverless__Fast_API):
+    enable_api_key = False
 
     def fast_api__title(self):                                       # todo: move this to the Fast_API class
         return FAST_API__TITLE
