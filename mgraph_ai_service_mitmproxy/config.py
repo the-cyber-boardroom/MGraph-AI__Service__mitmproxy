e36545870dd11bdd14a0f0cf5a407f6edc27e9ac
Dinis Cruz <dinis.cruz@owasp.org>
2025-09-05 07:18:16 +0100

    fixed dependenciew

--- mgraph_ai_service_mitmproxy/config.py
+++ mgraph_ai_service_mitmproxy/config.py
@@ -3,4 +3,4 @@ from mgraph_ai_service_mitmproxy import package_name
 SERVICE_NAME                             = package_name
 FAST_API__TITLE                          = "MGraph AI Service mitmproxy"
 FAST_API__DESCRIPTION                    = "Base template for MGraph-AI microservices"
-LAMBDA_DEPENDENCIES__FAST_API_SERVERLESS = ['osbot-fast-api-serverless==v1.2.0']
+LAMBDA_DEPENDENCIES__FAST_API_SERVERLESS = ['osbot-fast-api-serverless==1.17.0']
--- mgraph_ai_service_mitmproxy/fast_api/lambda_handler.py
+++ mgraph_ai_service_mitmproxy/fast_api/lambda_handler.py
@@ -1,11 +1,12 @@
 import os
 
+
 if os.getenv('AWS_REGION'):  # only execute if we are not running inside an AWS Lambda function
 
     from osbot_aws.aws.lambda_.boto3__lambda import load_dependencies       # using the lightweight file (which only has the boto3 calls required to load_dependencies)
-    LAMBDA_DEPENDENCIES =  ['osbot-fast-api-serverless==v1.2.0']
 
-    load_dependencies(LAMBDA_DEPENDENCIES)
+    from mgraph_ai_service_mitmproxy.config import LAMBDA_DEPENDENCIES__FAST_API_SERVERLESS
+    load_dependencies(LAMBDA_DEPENDENCIES__FAST_API_SERVERLESS)
 
     def clear_osbot_modules():                            # todo: add this to load_dependencies method, since after it runs we don't need the osbot_aws.aws.lambda_.boto3__lambda
         import sys
