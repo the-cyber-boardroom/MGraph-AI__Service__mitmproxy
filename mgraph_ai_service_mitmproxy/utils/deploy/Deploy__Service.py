b9814b04c514d65c39a33b6341d3e656c52620c3
Dinis Cruz <dinis.cruz@owasp.org>
2026-01-22 05:24:20 +0000

    wired in graph-service env vars in deployment

--- mgraph_ai_service_mitmproxy/utils/deploy/Deploy__Service.py
+++ mgraph_ai_service_mitmproxy/utils/deploy/Deploy__Service.py
@@ -23,7 +23,9 @@ class Deploy__Service(Deploy__Serverless__Fast_API):
             _.set_env_variable('AUTH__TARGET_SERVER__SEMANTIC_TEXT_SERVICE__KEY_NAME'  , get_env('AUTH__TARGET_SERVER__SEMANTIC_TEXT_SERVICE__KEY_NAME' ))
             _.set_env_variable('AUTH__TARGET_SERVER__SEMANTIC_TEXT_SERVICE__KEY_VALUE' , get_env('AUTH__TARGET_SERVER__SEMANTIC_TEXT_SERVICE__KEY_VALUE' ))
 
-
+            _.set_env_variable('AUTH__TARGET_SERVER__HTML_GRAPH_SERVICE__BASE_URL'  , get_env('AUTH__TARGET_SERVER__HTML_GRAPH_SERVICE__BASE_URL' ))
+            _.set_env_variable('AUTH__TARGET_SERVER__HTML_GRAPH_SERVICE__KEY_NAME'  , get_env('AUTH__TARGET_SERVER__HTML_GRAPH_SERVICE__KEY_NAME' ))
+            _.set_env_variable('AUTH__TARGET_SERVER__HTML_GRAPH_SERVICE__KEY_VALUE' , get_env('AUTH__TARGET_SERVER__HTML_GRAPH_SERVICE__KEY_VALUE' ))
 
 
             _.set_env_variable('WCF_SERVICE__AUTH__API_KEY__NAME'             , get_env('WCF_SERVICE__AUTH__API_KEY__NAME'  ))
