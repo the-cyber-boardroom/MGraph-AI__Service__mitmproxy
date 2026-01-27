c6df880f0627c01ff9e13f1dd71ac8f33d1d7084
Dinis Cruz <dinis.cruz@owasp.org>
2026-01-27 23:01:09 +0000

    updated lambda dependency

--- mgraph_ai_service_mitmproxy/config.py
+++ mgraph_ai_service_mitmproxy/config.py
@@ -7,7 +7,7 @@ LAMBDA_DEPENDENCIES__FAST_API_SERVERLESS = ['httpx==0.28.1'                     
                                             'memory-fs==v0.40.0'                      ,
                                             'mgraph-ai-service-cache-client==v0.32.0' ,
                                             'mgraph-ai-service-cache==v0.14.0'        ,
-                                            'mgraph-ai-service-html-graph==v1.7.0'    ,
+                                            'mgraph-ai-service-html-graph==v1.8.0'    ,
                                             'mgraph-db==v1.18.0'                      ,
                                             'osbot-fast-api-serverless==v1.33.0'      ]
 
