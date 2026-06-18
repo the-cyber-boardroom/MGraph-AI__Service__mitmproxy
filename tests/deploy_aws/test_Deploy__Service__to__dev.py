95292e2262b5b1eb316b1ec361388aa43fdbc7f0
Dinis Cruz <dinis.cruz@owasp.org>
2026-06-18 14:57:27 +0100

    Update test_Deploy__Service__to__dev.py

--- tests/deploy_aws/test_Deploy__Service__to__dev.py
+++ tests/deploy_aws/test_Deploy__Service__to__dev.py
@@ -8,14 +8,14 @@ from tests.deploy_aws.test_Deploy__Service__base import test_Deploy__Service__ba
 class test_Deploy__Service__to__dev(test_Deploy__Service__base, TestCase):
     stage = 'dev'
 
-    @classmethod
-    def setUpClass(cls):
-        dot_env_file = path_combine(__file__, "../.deploy.env")
-        assert file_exists(dot_env_file)
-        load_dotenv(dotenv_path=dot_env_file, override=True)
-        super().setUpClass()
+    # @classmethod
+    # def setUpClass(cls):
+    #     dot_env_file = path_combine(__file__, "../.deploy.env")
+    #     assert file_exists(dot_env_file)
+    #     load_dotenv(dotenv_path=dot_env_file, override=True)
+    #     super().setUpClass()
 
-    def test_4__invoke(self):
-        self.deploy_fast_api.create()
-        result = self.deploy_fast_api.invoke()
-        pprint(result)
+    # def test_4__invoke(self):
+    #     self.deploy_fast_api.create()
+    #     result = self.deploy_fast_api.invoke()
+    #     pprint(result)
